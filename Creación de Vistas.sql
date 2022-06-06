f266ad8f3d699e16b93d82110513d9bde85cc715
Santiago Chaves <92863260+S-Chaves@users.noreply.github.com>
2022-06-06 12:07:17 -0300

    Update Creación de Vistas.sql

--- Creación de Vistas.sql
+++ Creación de Vistas.sql
@@ -20,7 +20,7 @@ CREATE VIEW dinero_total_por_genero AS
 SELECT 
 	g.nombre AS Género,
     SUM(p.dinero_recaudado) AS DineroTotalRecaudado
-FROM generospeliculas gp
+FROM generos_peliculas gp
 	JOIN peliculas p ON (gp.id_pelicula = p.id_pelicula)
 	JOIN generos g ON (gp.id_genero = g.id_genero)
 GROUP BY g.nombre
@@ -33,20 +33,20 @@ SELECT
     p.puntaje AS PuntajePelícula,
     s.nombre AS Servicio,
     s.precio_mensual AS PrecioServicio
-FROM serviciospeliculas sp
+FROM servicios_peliculas sp
 	JOIN servicios s ON (sp.id_servicio = s.id_servicio)
 	JOIN peliculas p ON (sp.id_pelicula = p.id_pelicula)
 WHERE p.puntaje > 5
 ORDER BY p.puntaje DESC;
 
--- Permite ver la cantidad de actores y la suma de sus premios de las películas que tienen directores que dirigieron 
--- más de 15 películas.
+-- Permite ver la cantidad de actores y la suma de sus premios de las películas 
+-- que tienen directores que dirigieron más de 15 películas.
 CREATE VIEW cant_actores_y_premios_peliculas AS
 SELECT
 	p.nombre AS Película,
 	COUNT(a.nombre) AS CantidadActores,
     SUM(a.cantidad_premios) AS CantidadPremiosActores
-FROM actorespeliculas ap
+FROM actores_peliculas ap
 	JOIN actores a ON (ap.id_actor = a.id_actor)
 	JOIN peliculas p ON (ap.id_pelicula = p.id_pelicula)
 WHERE id_director IN 
@@ -54,4 +54,3 @@ WHERE id_director IN
 	WHERE cantidad_peliculas_dirigidas > 15)
 GROUP BY p.nombre
 ORDER BY CantidadPremiosActores DESC;
-
